f4e7a89e1a359d2c07274ee756747bcbbce1351f
hasan <Hasan.ossaily@hotmail.com>
2020-09-21 15:16:41 +0300

    push to remote respositery

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+    res.send("respond with a resource");
+});
+
+/* GET users listing. */
+router.get("/:id", function(req, res, next) {
+    res.send("respond with a " + req.params.id);
+});
+
+/* GET users listing. */
+router.get("/:id/app", function(req, res, next) {
+    res.send("respond with a app" + req.params.id);
+});
+
+module.exports = router;
